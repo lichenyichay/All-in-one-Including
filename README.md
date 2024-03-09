b45bc2e86d69873c31ffc6f81380575e7381a468
chay <lichenyi_2020@qq.com>
2024-03-09 21:55:10 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,39 @@
+# All-in-one2.4.0
+多功能一体机
+目前有1.4.2分支、2.4.0分支（正在紧急更新，最新）和安装包分支（installpackage）
+说明如下：
+```python
+Help on module Allinone:
+
+NAME
+    Allinone
+
+DESCRIPTION
+    # -*- coding:UTF-8 -*-
+    # @Author:Chay
+    # @TIME:2023/2/19 9:40:50
+    # @FILE:Allinone.py
+    # @version:2.4.0
+    # @Software: IntelliJ IDEA
+
+FUNCTIONS
+    allinone(fuwu)
+        :param 需要服务的功能
+        :return: 0：正常，1：不正常，其他返回值表示功能的结果
+        功能（按代码顺序排序，不分先后）：大小写互换、抽取随机数、求最小公倍数、求最大公倍数、图形计算器、小学学生信息管理系统、二分查找、求余、向下取整、向上取整、多个数求和、多个数求差、多个数求积、判断闰年、判断是否为质数、整数、小数计算（加减乘除）、分数计算（加减乘除）......（具体见Github All-in-one2.4.0分支Readme.md文件）
+
+DATA
+    jiebook = {}
+
+FILE
+    d:\chay\project\多功能一体机\all-in-one\2.4\src\allinone.py
+
+
+None
+```
+具体功能如下：
+大小写互换、抽取随机数、求最小公倍数、求最大公倍数、图形计算器、小学学生信息管理系统、二分查找、求余、向下取整、向上取整、多个数求和、多个数求差、多个数求积、判断闰年、判断是否为质数、整数、小数计算（加减乘除）、分数计算（加减乘除）、math库计算器、图书管理系统
+下载0.0.1-test版本（对应All-in-one1.4.0版本代码）
+https://github.com/lichenyichay/All-in-one1.4.0/releases/tag/v0.0.1-test
+下载0.0.2-test版本（对应All-in-one1.4.2版本代码）
+https://github.com/lichenyichay/All-in-one1.4.0/releases/tag/v0.0.2-test
---
+++ src/All-in-one_chayLichenyi/module/math_cal_py.py
@@ -0,0 +1,55 @@
+# -*- coding:UTF-8 -*-
+# @Author:Chay
+# @TIME:2022/12/24 15:38
+# @FILE:math_cal_py.py
+# @Software:IDLE 3.9.6
+import math
+'''
+函数名：math_cal
+调用形式：s = math_cal(mode,float1,float2)
+:param mode 模式 1~19 模式不在此选择范围内会抛出异常
+:param float1 float2 float型参数，根据模式的需求，如参数有空余，则空余参数添0即可，例：mode = 3:math_cal(3,5.2,0);
+例2：mode = 4:math_cal(4,0,0);例3：mode = 1:math_cal(1,5.92,-8)
+:return 结果
+'''
+def math_cal(mode,float1,float2):
+    if mode == 1:
+        return math.copysign(float1,float2)
+    elif mode == 2:
+        return math.cos(float1)
+    elif mode == 3:
+        return math.degrees(float1)
+    elif mode == 4:
+        return math.e
+    elif mode == 5:
+        return math.pi
+    elif mode == 6:
+        return math.tan(float1)
+    elif mode == 7:
+        return math.sqrt(float1)
+    elif mode == 8:
+        return math.sin(float1)
+    elif mode == 9:
+        return math.radians(float1)
+    elif mode == 10:
+        return math.pow(float1,float2)
+    elif mode == 11:
+        return math.modf(float1)
+    elif mode == 12:
+        return math.log(float1,float2)
+    elif mode == 13:
+        return math.ldexp(float1,float2)
+    elif mode == 14:
+        return not math.isnan(float1)
+    elif mode == 15:
+        return not math.isinf(float1)
+    elif mode == 16:
+        return math.factorial(int(float1))
+    elif mode == 17:
+        return math.fabs(float1)
+    elif mode == 18:
+        return math.exp1(float1)
+    elif mode == 19:
+        return math.exp(float1)
+    else:
+        raise TypeError("TypeError:类型错误！")
